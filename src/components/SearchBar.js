69633e4dd6d546d210b2c71d7957abc3d4c01b33
James Patrick Farrelly <james.farrelly69@mail.dcu.ie>
2021-06-05 13:45:15 +0100

    Connected youtube api

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -11,7 +11,7 @@ class SearchBar extends React.Component {
   onFormSubmit = (event) => {
     event.preventDefault();
 
-    // Todo make sure callback from parent componet is called
+    this.props.onFormSubmit(this.state.term);
   };
 
   render() {
