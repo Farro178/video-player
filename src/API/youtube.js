8db81a1ae4f6b928dda652651cdeb5a4ed67b24f
James Patrick Farrelly <james.farrelly69@mail.dcu.ie>
2021-06-05 15:13:34 +0100

    removed api key

--- src/API/youtube.js
+++ src/API/youtube.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const KEY = "AIzaSyBiyglHXYxcYQSztqI57fzsGrskNkoNOII";
+const KEY = "Insert Api key here";
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
