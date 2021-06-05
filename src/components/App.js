a9e16f2e8a705a762ca9068c50371dab02b28a5e
James Patrick Farrelly <james.farrelly69@mail.dcu.ie>
2021-06-05 15:09:10 +0100

    Completed basic video application

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,13 @@ import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../API/youtube";
 import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 class App extends React.Component {
-  state = { videos: [] };
+  state = { videos: [], selectedVideo: null };
+
+  componentDidMount() {
+    this.onTermSubmit("charli xcx");
+  }
 
   onTermSubmit = async (term) => {
     // accessing search endpoint
@@ -13,14 +18,34 @@ class App extends React.Component {
       },
     });
 
-    this.setState({ videos: response.data.items });
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0],
+    });
+  };
+
+  onVideoSelect = (video) => {
+    console.log("test", video);
+    this.setState({ selectedVideo: video });
   };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoList videos={this.state.videos} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
